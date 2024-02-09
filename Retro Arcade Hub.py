fb89ca5de22cf21827540147ef7e8181dba0b06c
Christian <124827807+Drone300@users.noreply.github.com>
2024-02-09 17:36:45 -0500

    Update Retro Arcade Hub.py

--- Retro Arcade Hub.py
+++ Retro Arcade Hub.py
@@ -1,38 +1,28 @@
 import tkinter as tk
 from tkinter import *
-
-def gui(win):
-    menu = Menu(win)
-    win.config(menu=menu,background="green")
-    filemenu = Menu(menu)
-    menu.add_cascade(label='File', menu=filemenu)
-    filemenu.add_command(label='New', command=main)
-    filemenu.add_command(label='Open...')
-    filemenu.add_separator()
-    filemenu.add_command(label='Exit', command=win.quit)
-    helpmenu = Menu(menu)
-    menu.add_cascade(label='Help', menu=helpmenu)
-    helpmenu.add_command(label='About')
+from BLackJack import table
 
 def button(win):
     frame = Frame(win)
     frame.pack()
     bottomframe = Frame(win)
     bottomframe.pack( side = BOTTOM )
-    redbutton = Button(bottomframe, text = 'BJ', fg ='Black', font="Arial")
-    redbutton.pack( side = LEFT)
+    blackbutton = Button(bottomframe, text = 'BlackJack', fg ='Black', font="Arial", command=table)
+    blackbutton.pack( side = LEFT)
     greenbutton = Button(bottomframe, text = 'Solitaire', fg='olive', font="Arial")
     greenbutton.pack( side = LEFT )
-    bluebutton = Button(bottomframe, text ='BattleShip', fg ='DarkRed',font="Arial")
-    bluebutton.pack( side = LEFT )
-
+    redbutton = Button(bottomframe, text ='BattleShip', fg ='DarkRed',font="Arial")
+    redbutton.pack( side = LEFT )
 def main():
     win = tk.Tk()
+    bg = PhotoImage(file="Blavck Hole 1.png")
+    label1 = Label(win, image=bg)
+    label1.place(x=0, y=0)
     win.title('Retro Arcade')
-    win.geometry("350x350")
+    win.geometry("500x600")
+    win.resizable(0,0)
     menu = Menu(win)
     win.config(menu=menu)
-    gui(win)
     button(win)
     win.mainloop()
 
